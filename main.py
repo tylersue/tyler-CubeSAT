2353de20770a2021da5a134ece3d55aee44dee78
tylersue <137845201+tylersue@users.noreply.github.com>
2024-09-25 09:13:07 -0700

    Add files via upload
    Fixed run.py to only output in main.py when "open" is typed in terminal. It used to run immediately after running main.py.

---
+++ main.py
@@ -0,0 +1,77 @@
+import os
+import sys
+import subprocess
+import run
+
+serial_commands = {}
+
+def serial_command(name):
+    def wrapped(callback):
+        serial_commands[name] = callback
+
+        return callback
+
+    return wrapped
+
+@serial_command("ping")
+def _(_):
+    print("pong")
+
+@serial_command("open")
+def _(_):
+    subprocess.Popen(["open", "-a", "Terminal"])  # Uncomment if needed
+    result = subprocess.run(["python3", "run.py"], capture_output=True, text=True)
+
+    print(result.stdout)
+    print("")
+
+def process_pc_command(cmd):
+    if len(cmd) < 1:
+        return
+
+    print("# Processing command:", cmd, "length", len(cmd))
+
+    args = cmd.split(" ")
+    command = args.pop(0)
+
+    if command not in serial_commands:
+        print("error: unrecognized command")
+        return
+
+    try:
+        serial_commands[command](args)
+    except Exception as e:
+        print("error: unexpected exception while processing command:", e)
+
+
+def check_for_pc_command():
+    serial_data_buffer = ""
+
+    while True:
+        data = sys.stdin.read(1)
+
+        if ord(data) == 27:
+            # delete, esc, arrow keys, etc.
+            # too difficult to handle, just drop the character
+
+            continue
+        elif ord(data) == 127:
+            # backspace
+            serial_data_buffer = serial_data_buffer[:-1]
+            print(data, end="")  # keep terminal state in sync
+
+            return
+
+        serial_data_buffer += data
+
+        # if serial_echo:
+        #     print(data, end="")
+
+        if data == "\n":
+            # command terminator
+            process_pc_command(serial_data_buffer.strip("\n").strip("\r"))
+            serial_data_buffer = ""
+
+
+while True:
+    check_for_pc_command()
---
+++ run.py
@@ -0,0 +1,12 @@
+# import time
+# while(True):
+#     print("ImgData")
+#     time.sleep(4)
+
+def main():
+    print("hi")
+    pass
+
+if __name__ == "__main__":
+    main()
+
